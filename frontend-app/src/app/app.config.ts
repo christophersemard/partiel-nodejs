29915e2db8fbe658ca7989d22f3b074a380a127f
Christopher Semard <equipe@bilbea.fr>
2025-02-06 18:17:26 +0100

    add tailwind

--- frontend-app/src/app/app.config.ts
+++ frontend-app/src/app/app.config.ts
@@ -2,27 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { providePrimeNG } from "primeng/config";
-import LaraLightBlue from "@primeng/themes/lara";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideAnimationsAsync(),
-        providePrimeNG({
-            theme: {
-                preset: LaraLightBlue,
-                options: {
-                    cssLayer: {
-                        name: "primeng",
-                        order: "tailwind-base, primeng, tailwind-utilities",
-                    },
-                },
-            },
-        }),
-
         provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes), provideAnimationsAsync(),
+        provideRouter(routes),
+        provideAnimationsAsync(),
     ],
 };
