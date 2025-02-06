a4157b6916aa2cbf27b5684849a16c79c28b3b24
Christopher Semard <equipe@bilbea.fr>
2025-02-06 19:29:47 +0100

    debut front

--- frontend-app/src/app/app.component.ts
+++ frontend-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, ViewEncapsulation } from "@angular/core";
+import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { LayoutComponent } from "./layout/layout.component";
 
 @Component({
     selector: "app-root",
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, LayoutComponent],
     templateUrl: "./app.component.html",
     styleUrl: "./app.component.scss",
 })
--- frontend-app/src/app/app.routes.ts
+++ frontend-app/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+import { ProductsComponent } from "./pages/products/products.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { RegisterComponent } from "./pages/register/register.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        children: [
+            { path: "", component: HomeComponent },
+            { path: "products", component: ProductsComponent },
+            { path: "login", component: LoginComponent },
+            { path: "register", component: RegisterComponent },
+        ],
+    },
+];
